9ae706935cc5cf2fd19b8ee6b42af8ccc17bd0e0
Didar1505 <didarkamiljanow15@gmail.com>
2025-12-19 15:01:22 +0300

    Добавлена таблица пользователей в БД

---
+++ База Данных Городских Автобусов/DataBase.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.Data.Sqlite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace База_Данных_Городских_Автобусов
+{
+    internal class DataBase
+    {
+        static string connectionString = "City-Bus.db";
+
+
+        public static void CreateAllTables()
+        {
+            CreateTableUsers();
+        }
+
+
+        private static void CreateTableUsers()
+        {
+            SqliteConnection conn = new SqliteConnection(connectionString);
+            try
+            {
+                conn.Open();
+                SqliteCommand cmd = new SqliteCommand();
+                cmd.Connection = conn;
+                cmd.CommandText = @"CREATE TABLE IF NOT EXISTS Users (
+                                   user_id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+                                   username TEXT NOT NULL UNIQUE,
+                                   password_hash TEXT NOT NULL,
+                                   full_name TEXT NOT NULL,
+                                   role TEXT NOT NULL,
+                                   is_active BOOLEAN DEFAULT 1,
+                                   created_date DATETIME DEFAULT CURRENT_TIMESTAMP)";
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Таблица пользователей создана успешно!", "Успех", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка создания таблицы пользователей: {ex}");
+            }
+            finally
+            {
+                conn.Close();
+            }
+
+
+        }
+
+
+
+
+
+    }
+}
